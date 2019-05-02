70cc7776d5cb61aa2ba29f5e06a776437122b14c
Fabio <fa.espinosa10@uniandes.edu.co>
2019-05-02 16:58:39 +0200

    fix travis build

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,10 @@
-from runregistry.utils import transform_to_rr_filter
+from runregistry.utils import transform_to_rr_run_filter
 
 
 class TestFilterCreation:
     def test_get_run(self):
         run_number = 323434
-        assert transform_to_rr_filter(run_filter={'run_number': run_number}) == {
+        assert transform_to_rr_run_filter(run_filter={'run_number': run_number}) == {
             'oms_attributes.run_number': {'=': run_number}}
 
     def test_get_multiple_run_using_or(self):
@@ -28,4 +28,5 @@ class TestFilterCreation:
             }
         }
 
-        assert transform_to_rr_filter(run_filter=user_input) == desired_output
+        assert transform_to_rr_run_filter(
+            run_filter=user_input) == desired_output
